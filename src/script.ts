8132293c082c1f141172af4633ee1fe5c87db8e0
David92p <Dave.panetta92@gmail.com>
2023-07-18 22:06:04 +0200

    import and edit select list update functions

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,10 @@
-import { createProducts } from "./functions/createProducts.js";
-import { createQuantity } from "./functions/createQuantity.js";
+import { LIST_MEASURES } from "./data/list_measures.js";
+import { LIST_PRODUCTS } from "./data/list_products.js";
+import { setListMeasures } from "./functions/setListMeasures.js";
+import { setListProducts } from "./functions/setListProducts.js";
 
 const form = document.querySelector("form") as HTMLFormElement;
-const type = document.querySelector("#tipologia") as HTMLSelectElement;
+const typology = document.querySelector("#tipologia") as HTMLSelectElement;
 const products = document.querySelector("#prodotto") as HTMLSelectElement;
 const quantity = document.querySelector("#quantita") as HTMLInputElement;
 const quantityType = document.querySelector(
@@ -10,18 +12,30 @@ const quantityType = document.querySelector(
 ) as HTMLSelectElement;
 const btn = document.querySelector("button") as HTMLButtonElement;
 
-type.addEventListener("change", (e: Event) => {
+typology.addEventListener("change", (e: Event) => {
   e.preventDefault();
-  createProducts(type.value, products);
+  setListProducts({
+    type: typology.value,
+    select: products,
+    list: LIST_PRODUCTS
+  });
 });
 
 products.addEventListener("change", (e: Event) => {
   e.preventDefault();
-  createQuantity(type.value, quantityType, products.value);
-  console.log(products.type);
+  setListMeasures({
+    type: products.value,
+    select: quantityType,
+    list: LIST_MEASURES
+  });
 });
 
-btn.addEventListener("click", (e: Event) => {
-  e.preventDefault();
-  console.log(type.value, products.value, quantity.value, quantityType.value);
-});
+// btn.addEventListener("click", (e: Event) => {
+//   e.preventDefault();
+//   console.log(
+//     typology.value,
+//     products.value,
+//     quantity.value,
+//     quantityType.value
+//   );
+// });

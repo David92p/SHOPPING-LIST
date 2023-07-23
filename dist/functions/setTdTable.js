4ef1eb7eff70f0424c6fc82108560642afd52a0e
David92p <Dave.panetta92@gmail.com>
2023-07-23 21:37:52 +0200

    added form submission conditions

--- dist/functions/setTdTable.js
+++ dist/functions/setTdTable.js
@@ -1,5 +1,23 @@
 export const setTdTable = (arg) => {
     console.log(arg);
+    if (!arg.typology.value) {
+        arg.typology.style.border = "2px solid red";
+        alert("Inrerisci una tipologia di prodotto per proseguire");
+        // let span = document.createElement("span");
+        // span.classList.add("error-type");
+        // span.innerText = "Seleziona una tipologia";
+        // arg.typology.parentNode?.insertBefore(span, arg.typology);
+    }
+    else {
+        if (!arg.quantity.value) {
+            arg.quantity.style.border = "2px solid red";
+            alert("Inserisci la quantità");
+        }
+        if (arg.quantityType.value == "---") {
+            arg.quantityType.style.border = "2px solid red";
+            alert("Inserisci la tipologia di quantità");
+        }
+    }
     //   if (arg.typology.value == "Seleziona prodotto") {
     //     arg.typology.style.border = "2px solid red";
     //   } else {

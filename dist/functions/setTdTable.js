d185593251ce9e870fc3786999ff428861cc84b0
David92p <dave.panetta92@gmail.com>
2023-07-24 16:59:06 +0200

    Updated table update function

--- dist/functions/setTdTable.js
+++ dist/functions/setTdTable.js
@@ -1,33 +1,48 @@
 export const setTdTable = (arg) => {
-    console.log(arg);
+    var _a;
     if (!arg.typology.value) {
-        arg.typology.style.border = "2px solid red";
+        arg.typology.style.border = "3px solid red";
         alert("Inrerisci una tipologia di prodotto per proseguire");
-        // let span = document.createElement("span");
-        // span.classList.add("error-type");
-        // span.innerText = "Seleziona una tipologia";
-        // arg.typology.parentNode?.insertBefore(span, arg.typology);
     }
     else {
-        if (!arg.quantity.value) {
-            arg.quantity.style.border = "2px solid red";
-            alert("Inserisci la quantità");
+        if (!arg.quantity.value || arg.quantityType.value == "---") {
+            alert("Inserisci i dati mancanti");
+            if (!arg.quantity.value)
+                arg.quantity.style.border = "3px solid red";
+            if (arg.quantityType.value == "---")
+                arg.quantityType.style.border = "3px solid red";
         }
-        if (arg.quantityType.value == "---") {
-            arg.quantityType.style.border = "2px solid red";
-            alert("Inserisci la tipologia di quantità");
+        else {
+            console.log(arg);
+            let tr = document.createElement("tr");
+            let tdTipology = document.createElement("td");
+            tdTipology.innerText =
+                arg.typology.value.charAt(0).toUpperCase() +
+                    arg.typology.value.slice(1);
+            let tdProduct = document.createElement("td");
+            tdProduct.innerText = arg.products.value;
+            let tdQuantity = document.createElement("td");
+            tdQuantity.innerText =
+                arg.quantity.value + " - " + arg.quantityType.value;
+            let tdNote = document.createElement("td");
+            tdNote.innerText = arg.textArea.value;
+            let tdButton = document.createElement("td");
+            let checkButton = document.createElement("button");
+            let checkImg = document.createElement("img");
+            checkImg.src = "./utilities/check.png";
+            checkButton.appendChild(checkImg);
+            let deleteButton = document.createElement("button");
+            let deleteImg = document.createElement("img");
+            deleteImg.src = "./utilities/delete.png";
+            deleteButton.appendChild(deleteImg);
+            tdButton.appendChild(checkButton);
+            tdButton.appendChild(deleteButton);
+            tr.appendChild(tdTipology);
+            tr.appendChild(tdProduct);
+            tr.appendChild(tdQuantity);
+            tr.appendChild(tdNote);
+            tr.appendChild(tdButton);
+            (_a = arg.table.tFoot) === null || _a === void 0 ? void 0 : _a.appendChild(tr);
         }
     }
-    //   if (arg.typology.value == "Seleziona prodotto") {
-    //     arg.typology.style.border = "2px solid red";
-    //   } else {
-    //     arg.typology.style.border = "1px solid black";
-    //   }
-    //   return {
-    //     typology: "string",
-    //     products: "string",
-    //     quantity: 5,
-    //     quantityType: "string",
-    //     textArea: "string",
-    //   };
 };
--- src/functions/setTdTable.ts
+++ src/functions/setTdTable.ts
@@ -1,36 +1,52 @@
 import { TdTable } from "../interfaces/tdTable";
 
 export const setTdTable = (arg: TdTable) => {
-  console.log(arg);
-  if (!arg.typology.value){
-    arg.typology.style.border = "2px solid red";
-    alert("Inrerisci una tipologia di prodotto per proseguire")
-    // let span = document.createElement("span");
-    // span.classList.add("error-type");
-    // span.innerText = "Seleziona una tipologia";
-    // arg.typology.parentNode?.insertBefore(span, arg.typology);
-  }else {
-    if (!arg.quantity.value){ 
-      arg.quantity.style.border = "2px solid red" 
-      alert("Inserisci la quantità")
+  if (!arg.typology.value) {
+    arg.typology.style.border = "3px solid red";
+    alert("Inrerisci una tipologia di prodotto per proseguire");
+  } else {
+    if (!arg.quantity.value || arg.quantityType.value == "---") {
+      alert("Inserisci i dati mancanti");
+      if (!arg.quantity.value) arg.quantity.style.border = "3px solid red";
+      if (arg.quantityType.value == "---")
+        arg.quantityType.style.border = "3px solid red";
+    } else {
+      console.log(arg);
+
+      let tr = document.createElement("tr");
+
+      let tdTipology = document.createElement("td");
+      tdTipology.innerText =
+        arg.typology.value.charAt(0).toUpperCase() +
+        arg.typology.value.slice(1);
+      let tdProduct = document.createElement("td");
+      tdProduct.innerText = arg.products.value;
+      let tdQuantity = document.createElement("td");
+      tdQuantity.innerText =
+        arg.quantity.value + " - " + arg.quantityType.value;
+      let tdNote = document.createElement("td");
+      tdNote.innerText = arg.textArea.value;
+      let tdButton = document.createElement("td");
+
+      let checkButton = document.createElement("button") as HTMLButtonElement;
+      let checkImg = document.createElement("img") as HTMLImageElement;
+      checkImg.src = "./utilities/check.png";
+      checkButton.appendChild(checkImg);
+
+      let deleteButton = document.createElement("button") as HTMLButtonElement;
+      let deleteImg = document.createElement("img") as HTMLImageElement;
+      deleteImg.src = "./utilities/delete.png";
+      deleteButton.appendChild(deleteImg);
+      tdButton.appendChild(checkButton);
+      tdButton.appendChild(deleteButton);
+
+      tr.appendChild(tdTipology);
+      tr.appendChild(tdProduct);
+      tr.appendChild(tdQuantity);
+      tr.appendChild(tdNote);
+      tr.appendChild(tdButton);
+
+      arg.table.tFoot?.appendChild(tr);
     }
-    if (arg.quantityType.value == "---"){
-      arg.quantityType.style.border = "2px solid red";
-      alert("Inserisci la tipologia di quantità")
-    } 
-    
   }
-  
-  //   if (arg.typology.value == "Seleziona prodotto") {
-  //     arg.typology.style.border = "2px solid red";
-  //   } else {
-  //     arg.typology.style.border = "1px solid black";
-  //   }
-  //   return {
-  //     typology: "string",
-  //     products: "string",
-  //     quantity: 5,
-  //     quantityType: "string",
-  //     textArea: "string",
-  //   };
 };
